9a87ca64a524807775d91109558ecad8474e0305
Jesus Leyva <jesusleyva066@gmail.com>
2023-09-16 19:30:30 -0700

    Experiencia completo y medio responsivo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-$(document).ready(function() {
-    $(".progress-bar").each(function() {
-        var progress = $(this).attr("data-progress");
-        $(this).animate({ width: progress + "%" }, 1000); // 1000ms (1 segundo) de animación
-    });
+$(document).ready(function () {
+  $(".progress-bar").each(function () {
+    var progress = $(this).attr("data-progress");
+    $(this).animate({ width: progress + "%" }, 1000); // 1000ms (1 segundo) de animación
+  });
+});
+
+const hamburguer = document.querySelector(".hamburguer");
+const navLinks = document.querySelector(".nav-links");
+
+hamburguer.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("show");
 });
